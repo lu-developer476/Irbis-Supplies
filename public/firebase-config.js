483f5a2f38e073ab34249c4ad7615c80068b0e53
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-02 00:01:46 -0300

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -12,5 +12,8 @@ window.IRBIS_FIREBASE_CONFIG = {
   apiKey: "AIzaSyCUPhvED65134MnJ2EYQyTEzfxSXueTpOM",
   authDomain: "irbis-supplies.firebaseapp.com",
   projectId: "irbis-supplies",
+  storageBucket: "irbis-supplies.firebasestorage.app",
+  messagingSenderId: "885163922238",
   appId: "1:885163922238:web:f04fa437d36fb69143526f",
+  measurementId: "G-E4VNP5E822"
 };
